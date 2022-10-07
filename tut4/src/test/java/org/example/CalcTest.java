061274ba13ca28d60c5e55f3f2bb23644a9a97c0
parieur <112766090+parieur@users.noreply.github.com>
2022-10-07 21:42:34 +0800

    tut4

---
+++ tut4/src/test/java/org/example/CalcTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalcTest {
+    @Test
+    void testAddition() {
+        Calc c = new Calc();
+        assertEquals(4, c.add(2,2));
+    }
+@Test
+void testSubtract(){
+Calc c = new Calc();
+assertEquals(2, c.subtract(4,2));}
+}
